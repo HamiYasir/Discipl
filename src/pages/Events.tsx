0c36b8392c39f85e4dac60820b87e0e1bab6f548
Hami Yasir <hamiyasir06@gmail.com>
2025-09-26 15:39:36 +0530

    commit #28
    - Resolved deployment issue due to typo in API

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -114,7 +114,7 @@ const Events: React.FC = () => {
         paymentId: paymentDetails.response.razorpay_payment_id
       };
 
-      await axios.post('https://discipl-server.onrender.com/api/particpants/add', payload); // This is used when running from github repo
+      await axios.post('https://discipl-server.onrender.com/api/participants/add', payload); // This is used when running from github repo
       // await axios.post("http://localhost:8172/api/participants/add", payload); // This is used when running on localhost
       // alert("Participant registered successfully!"); //DEBUG
       setIsParticipantModalOpen(false);
